73956378c2c2583e875b7f83b664d544f3ca6d70
Aaryan Bajaj <aaryanbajaj385@gmail.com>
2024-07-20 11:23:48 +0530

    added bank schema

--- backend/db.js
+++ backend/db.js
@@ -3,25 +3,41 @@ const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/bajpe");
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-        minLength: [3, "Username must be at least 3 characters long"]
-    },
-    firstname: {
-        type: String,
-        required: true
-    },
-    lastname: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+  username: {
+    type: String,
+    required: true,
+    minLength: [3, "Username must be at least 3 characters long"],
+  },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const accountSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
 
 const User = mongoose.model("User", userSchema);
+const Account = mongoose.model("Account", accountSchema);
 
-module.exports = User
+module.exports = {
+	User,
+  Account,
+};
